a4c54866f5a495d4fa05daa2aae21e7024e9b53e
Mike-Valencia <espanyol.mike@gmail.com>
2016-04-20 20:28:07 -0300

    CirculoMike.

---
+++ src/ar/edu/tw1tp1/unlam/www/Circulo.java
@@ -0,0 +1,11 @@
+package ar.edu.tw1tp1.unlam.www;
+
+public class Circulo {
+	private Double area;
+	
+	public Double calcularArea(Double radio){
+		area = 3.14 * (radio*radio) ;
+		return area;
+		
+	}
+}
